19cac046f592e9e097f0a52accde4a9dde5c44c7
cza14h <chen_ziao@126.com>
2023-10-27 00:37:21 +0800

    feat: add calc node logic

--- src/lib/utils/rearrange.ts
+++ src/lib/utils/rearrange.ts
@@ -16,6 +16,8 @@ type Options = {
   };
 };
 
+const MOVE_UNIT = 1;
+
 const step = (
   stepNumber: number,
   iterNum: number,
@@ -89,27 +91,26 @@ const arrangeRelax = (
   distance: number,
   clampedPull: boolean,
 ) => {
-  const currentNodeAtom = nodeAtomStates[nodeId];
-  const currentNode = currentNodeAtom.node;
-  const currentNodeCoor = getGlobalLocation(currentNode);
-  const currentNodeRect = currentNodeAtom.rect;
+  const nodeState = nodeAtomStates[nodeId];
+  const nodeCoor = getGlobalLocation(nodeState.node);
+  const nodeRect = nodeState.rect;
 
   const offset: coordinates = { x: 0, y: 0 };
 
   let tarY = 0,
-    tarXIn = clampedPull ? currentNodeCoor.x : 0,
+    tarXIn = clampedPull ? nodeCoor.x : 0,
     linkCnt = 0,
     hasInput = false;
 
-  const inputs = currentNodeAtom.handles.target;
+  const inputs = nodeState.handles.target;
 
   for (const handle in inputs) {
     const edges = [...edgeTree.get(nodeId)!.get(handle)!.values()];
 
     for (const edge of edges) {
-      const upstreamNodeAtom = nodeAtomStates[edge.sourceNode];
-      const upsteamNodeCoor = getGlobalLocation(upstreamNodeAtom.node);
-      const upsteamNodeRect = { ...upstreamNodeAtom.rect };
+      const upstreamNode = nodeAtomStates[edge.sourceNode];
+      const upsteamNodeCoor = getGlobalLocation(upstreamNode.node);
+      const upsteamNodeRect = { ...upstreamNode.rect };
       const x = upsteamNodeCoor.x + upsteamNodeRect.width + distance;
 
       if (clampedPull) {
@@ -120,23 +121,23 @@ const arrangeRelax = (
       hasInput = true;
       tarY +=
         upsteamNodeCoor.y +
-        socketPosition(handle, inputs, currentNodeRect.height) -
+        socketPosition(handle, inputs, nodeRect.height) -
         socketPosition(link.from_socket, other.outputs, upsteamNodeRect.height);
       linkCnt++;
     }
   }
 
-  const tarXOut = clampedPull ? currentNodeCoor.x : 0;
+  const tarXOut = clampedPull ? nodeCoor.x : 0;
   let hasOutput = false;
 
-  for (const handle in currentNodeAtom.handles.source) {
+  for (const handle in nodeState.handles.source) {
     const edges = [...edgeTree.get(nodeId)!.get(handle)!.values()];
 
     for (const edge of edges) {
-      const downstreamNodeAtom = nodeAtomStates[edge.targetNode];
-      const downstreamNodeCoor = getGlobalLocation(downstreamNodeAtom.node);
-      const downstreamNodeRect = { ...downstreamNodeAtom.rect };
-      const x = downstreamNodeCoor.x - currentNodeRect.width - distance;
+      const downstreamNode = nodeAtomStates[edge.targetNode];
+      const downstreamNodeCoor = getGlobalLocation(downstreamNode.node);
+      const downstreamNodeRect = { ...downstreamNode.rect };
+      const x = downstreamNodeCoor.x - nodeRect.width - distance;
 
       if (clampedPull) {
         tarXIn = hasOutput ? Math.min(tarXOut, x) : x;
@@ -146,7 +147,7 @@ const arrangeRelax = (
       hasOutput = true;
       tarY +=
         downstreamNodeCoor.y +
-        socketPosition(socket, node.inputs, currentNodeRect.y) -
+        socketPosition(socket, node.inputs, nodeRect.y) -
         socketPosition(link.from_socket, other.outputs, downstreamNodeRect.height);
       linkCnt++;
     }
@@ -161,10 +162,10 @@ const arrangeRelax = (
       tarX = (tarXIn + tarXOut) / linkCnt;
     }
     tarY /= linkCnt;
-    offset.x += (tarX - currentNodeCoor.x) * relaxPower;
-    offset.y += (tarY - currentNodeCoor.y) * relaxPower;
+    offset.x += (tarX - nodeCoor.x) * relaxPower;
+    offset.y += (tarY - nodeCoor.y) * relaxPower;
 
-    if (Math.abs(offset.x) > 1 || Math.abs(offset.y) > 1) {
+    if (Math.abs(offset.x) > MOVE_UNIT || Math.abs(offset.y) > MOVE_UNIT) {
       applyMovement(nodeId, nodeAtomStates, {
         x: offset.x * influence,
         y: offset.y * influence,
@@ -179,3 +180,104 @@ const applyMovement = (
   nodeAtomStates: Record<string, NodeAtomState>,
   offset: coordinates,
 ) => {};
+
+const calcNode = (
+  edgeTree: EdgeTree,
+  nodeId: string,
+  nodeAtomStates: Record<string, NodeAtomState>,
+  influence: number,
+  slideVector: coordinates,
+  relaxPower: number,
+  collidePower: number,
+  collideDist: coordinates,
+  pullNonSiblings: boolean,
+) => {
+  const nodeState = nodeAtomStates[nodeId];
+  if (nodeState.node.type === '') {
+    return false;
+  }
+
+  const nodeCoor = getGlobalLocation(nodeState.node);
+  const nodeRect = nodeState.rect;
+  const offset = { ...slideVector };
+
+  if (relaxPower > 0) {
+    let tarY = 0,
+      linkCnt = 0,
+      tarXIn = nodeCoor.x,
+      hasInput = false;
+
+    const inputs = nodeState.handles.target;
+    for (const handle in inputs) {
+      const edges = [...edgeTree.get(nodeId)!.get(handle)!.values()];
+      for (const edge of edges) {
+        const upstreamNodeAtom = nodeAtomStates[edge.targetNode];
+
+        // parent
+        // if (!pullNonSiblings && nodeState.parent !== upstreamNodeAtom.parent) {
+        //   continue;
+        // }
+
+        const upstreamNodeCoor = getGlobalLocation(upstreamNodeAtom.node);
+        const upstreamNodeRect = { ...upstreamNodeAtom.rect };
+        const x = upstreamNodeCoor.x + upstreamNodeRect.width + collideDist.x;
+
+        tarXIn = Number(hasInput) > 0 ? Math.max(tarXIn, x) : x;
+        hasInput = true;
+        tarY += upstreamNodeCoor.y - upstreamNodeRect.height / 2;
+        linkCnt++;
+      }
+    }
+
+    let tarXOut = nodeCoor.x,
+      hasOutput = false;
+
+    const outputs = nodeState.handles.source;
+    for (const handle in outputs) {
+      const edges = [...edgeTree.get(nodeId)!.get(handle)!.values()];
+      for (const edge of edges) {
+        const downstreamNode = nodeAtomStates[edge.targetNode];
+        // if (!pullNonSiblings && nodeState.parent !== downstreamNodeAtom.parent) {
+        //   continue;
+        // }
+        const downstreamNodeCoor = getGlobalLocation(downstreamNode.node);
+        const downstreamNodeRect = { ...downstreamNode.rect };
+
+        const x = downstreamNodeCoor.x - nodeRect.width - collideDist.x;
+
+        tarXOut = Number(hasOutput) ? Math.min(tarXOut, x) : x;
+        hasOutput = true;
+
+        tarY += downstreamNodeCoor.y - downstreamNodeRect.height / 2;
+        linkCnt += 1;
+      }
+    }
+
+    if (linkCnt > 0) {
+      let tarX = tarXIn * Number(hasInput) + tarXOut * Number(hasOutput);
+      tarX /= Number(hasInput) + Number(hasOutput);
+      tarY /= linkCnt;
+      tarY += nodeRect.height / 2;
+      offset.x += (tarX - nodeCoor.x) * relaxPower;
+      offset.y += (tarY - nodeCoor.y) * relaxPower;
+    }
+  }
+
+  if (collidePower > 0) {
+    for (const node in nodeAtomStates) {
+      if (node === nodeId) continue;
+      const nodeState = nodeAtomStates[node];
+      // if (nodeState.node.type === '') continue;
+    }
+  }
+
+  if (Math.abs(offset.x) > MOVE_UNIT || Math.abs(offset.y) > MOVE_UNIT) {
+    applyMovement(nodeId, nodeAtomStates, {
+      x: offset.x * influence,
+      y: offset.y * influence,
+    });
+    return true;
+  } else {
+    return false;
+  }
+};
